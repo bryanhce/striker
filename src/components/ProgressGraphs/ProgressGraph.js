8c2b4c132e8889f9174d989e8c7c95286f066a97
Bryan <ho.cheng.en.bryan@gmail.com>
2022-05-25 18:02:03 +0800

    adding static progress page

---
+++ src/components/ProgressGraphs/ProgressGraph.js
@@ -0,0 +1,38 @@
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLabel,
+} from "victory";
+import "./ProgressGraph.css";
+
+const ProgressGraph = (props) => {
+  return (
+    <div className="progress-graph">
+      <h2>{props.title}</h2>
+      <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}
+        style={{ parent: { maxWidth: "200%", width: "150%" } }}
+      >
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4, 5, 6]}
+          tickFormat={["Jan", "Feb", "Mar", "Apr", "May", "Jun"]}
+        />
+        <VictoryAxis dependentAxis tickFormat={(x) => `${x}%`} />
+        <VictoryBar
+          data={props.data}
+          x="month"
+          y="productivity"
+          animate
+          labelComponent={<VictoryLabel />}
+          labels={props.labelArr}
+          style={{ labels: { fill: "black", fontSize: "9" } }}
+        />
+      </VictoryChart>
+    </div>
+  );
+};
+
+export default ProgressGraph;
--- src/pages/Progress/ProgressPage.js
+++ src/pages/Progress/ProgressPage.js
@@ -1,5 +1,94 @@
+import StrikerLayout from "../StrikerLayout/StrikerLayout";
+import { Card, Col, Row, Progress } from "antd";
+import "./ProgressPage.css";
+import ProgressGraph from "../../components/ProgressGraphs/ProgressGraph";
+
+const productivityData = [
+  { month: 1, productivity: 60 },
+  { month: 2, productivity: 83 },
+  { month: 3, productivity: 3 },
+  { month: 4, productivity: 45 },
+  { month: 5, productivity: 100 },
+  { month: 6, productivity: 32 },
+];
+
+const productivityLabelArr = productivityData.map(
+  (obj) => obj.productivity.toString() + "%"
+);
+
+const CompletionData = [
+  { month: 1, productivity: 34 },
+  { month: 2, productivity: 50 },
+  { month: 3, productivity: 97 },
+  { month: 4, productivity: 88 },
+  { month: 5, productivity: 67 },
+  { month: 6, productivity: 49 },
+];
+
+const CompletionLabelArr = CompletionData.map(
+  (obj) => obj.productivity.toString() + "%"
+);
+
 const ProgressPage = () => {
-  return <div>Progress</div>;
+  return (
+    <StrikerLayout>
+      <div className="site-card-wrapper">
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card title="Productivity/ Effort Rate" className="progress-card">
+              <Progress type="circle" percent={89} strokeColor={"#a0d911"} />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Completion Rate" className="progress-card">
+              <Progress type="circle" percent={77} />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Productive Days Per Year" className="progress-card">
+              <Progress
+                type="dashboard"
+                format={() => "37 Days"}
+                percent={20}
+                strokeColor="orange"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div className="site-card-wrapper">
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card title="Total Tasks Completed" className="progress-card">
+              109 Tasks
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Total Reminders" className="progress-card">
+              77 Reminders
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Total Events" className="progress-card">
+              42 Events
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div className="graph-div">
+        <ProgressGraph
+          title="Graph of Productivity Per Month"
+          data={productivityData}
+          labelArr={productivityLabelArr}
+        />
+        <ProgressGraph
+          title="Graph of Completion Per Month"
+          data={CompletionData}
+          labelArr={CompletionLabelArr}
+        />
+      </div>
+    </StrikerLayout>
+  );
 };
 
 export default ProgressPage;
