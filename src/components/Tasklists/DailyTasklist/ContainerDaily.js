00bea207f3a2801e93b389f94e4713b05fa8b0eb
Bryan <ho.cheng.en.bryan@gmail.com>
2022-07-02 19:28:12 +0800

    removing yesterday reminder for ms2

--- src/components/Tasklists/DailyTasklist/ContainerDaily.js
+++ src/components/Tasklists/DailyTasklist/ContainerDaily.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import AddTask from '../AddTask';
-import Progress from '../Progress';
-import TableDaily from './TableDaily';
+import React from "react";
+import AddTask from "../AddTask";
+import ProgressBar from "../Progress";
+import TableDaily from "./TableDaily";
 
-export const ContainerDaily = ({ tasks, strikeTask, deleteTask, addTask, filterPriority, filterEffort, filters, updateTaskTextState, updateTaskEffortState, changeTaskType, changeTaskPriority, updateTaskTypeEvent, updateTaskTextEvent, updateTaskPriorityEvent, updateTaskEffortEvent }) => {
-    const daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-    var today = new Date();
-    return (
-        <div className="border">
-            <div className="border-header">
-                <h2 className="border-day">{daysOfWeek[today.getDay() - 1]}</h2>
-                <h2 className="border-date">{today.getDate() + " . " + today.getMonth() + " . " + today.getFullYear()}</h2>
-            </div>
-            <TableDaily tasks={tasks} strikeTask={strikeTask} deleteTask={deleteTask} 
-            filterPriority={filterPriority} filterEffort={filterEffort} filters={filters}
-            updateTaskTextState={updateTaskTextState} updateTaskEffortState={updateTaskEffortState} changeTaskType={changeTaskType} changeTaskPriority={changeTaskPriority}
-            updateTaskTypeEvent={updateTaskTypeEvent} updateTaskTextEvent={updateTaskTextEvent} updateTaskPriorityEvent={updateTaskPriorityEvent} updateTaskEffortEvent={updateTaskEffortEvent}/>
-            <AddTask addTask={addTask} />
-            <Progress tasks={tasks}/>
-        </div>
-    )
-}
+export const ContainerDaily = ({
+  tasks,
+  strikeTask,
+  deleteTask,
+  addTask,
+  filterPriority,
+  filterEffort,
+  filters,
+  updateTaskTextState,
+  updateTaskEffortState,
+  changeTaskType,
+  changeTaskPriority,
+  updateTaskTypeEvent,
+  updateTaskTextEvent,
+  updateTaskPriorityEvent,
+  updateTaskEffortEvent,
+}) => {
+  const daysOfWeek = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  var today = new Date();
+  return (
+    <div className="border">
+      <div className="border-header">
+        <h2 className="border-day">{daysOfWeek[today.getDay() - 1]}</h2>
+        <h2 className="border-date">
+          {today.getDate() +
+            " . " +
+            today.getMonth() +
+            " . " +
+            today.getFullYear()}
+        </h2>
+      </div>
+      <TableDaily
+        tasks={tasks}
+        strikeTask={strikeTask}
+        deleteTask={deleteTask}
+        filterPriority={filterPriority}
+        filterEffort={filterEffort}
+        filters={filters}
+        updateTaskTextState={updateTaskTextState}
+        updateTaskEffortState={updateTaskEffortState}
+        changeTaskType={changeTaskType}
+        changeTaskPriority={changeTaskPriority}
+        updateTaskTypeEvent={updateTaskTypeEvent}
+        updateTaskTextEvent={updateTaskTextEvent}
+        updateTaskPriorityEvent={updateTaskPriorityEvent}
+        updateTaskEffortEvent={updateTaskEffortEvent}
+      />
+      <AddTask addTask={addTask} />
+      <ProgressBar tasks={tasks} />
+    </div>
+  );
+};
