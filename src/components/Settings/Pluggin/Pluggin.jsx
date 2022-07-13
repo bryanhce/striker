841e0889e35f64b8b3284edc5f4cb28aa6e8cf55
Bryan <ho.cheng.en.bryan@gmail.com>
2022-07-13 23:02:17 +0800

    create 135 feature

--- src/components/Settings/Pluggin/Pluggin.jsx
+++ src/components/Settings/Pluggin/Pluggin.jsx
@@ -1,9 +1,41 @@
-import { Switch } from "antd";
-import { Fragment } from "react";
+import { Switch, Space, Button, notification } from "antd";
+import { Fragment, useState } from "react";
 import { usePomodoro } from "../../../context/PomodoroContext";
+import { use135 } from "../../../context/OneThreeFiveContext";
 
 const Pluggin = () => {
-  const { togglePomoButton } = usePomodoro();
+  const { togglePomoButton, setPomoTime, pomoTime } = usePomodoro();
+  const { toggle135Button } = use135();
+
+  const [tempTime, setTempTime] = useState(pomoTime);
+  const handleChange = (e) => {
+    setTempTime(e.target.value);
+  };
+
+  const changePomoTime = () => {
+    if (tempTime < 1 || tempTime > 60) {
+      errorNotification();
+    } else {
+      setPomoTime(tempTime);
+      successNotification();
+    }
+  };
+
+  const errorNotification = () => {
+    notification["error"]({
+      message: "Pomodoro Time error",
+      description:
+        "Please ensure that the value inputted is between 1 and 60 minutes.",
+    });
+  };
+
+  const successNotification = () => {
+    notification["success"]({
+      message: "Pomodoro Time change successful",
+      description: "Pomodoro time has been set to your new preference.",
+    });
+  };
+
   return (
     <Fragment>
       <div style={{ fontSize: "20px" }}>
@@ -14,9 +46,13 @@ const Pluggin = () => {
       <br />
       <div style={{ display: "flex", flexDirection: "row" }}>
         <h3 style={{ paddingRight: "1%" }}>
-          <strong>1-3-5</strong>
+          <strong>1-3-5 Framework</strong>
         </h3>
-        <Switch defaultChecked={false} autoFocus={true} />
+        <Switch
+          defaultChecked={false}
+          autoFocus={true}
+          onChange={toggle135Button}
+        />
       </div>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <h3 style={{ textDecoration: "underline" }}>Philosophy</h3>
@@ -32,6 +68,10 @@ const Pluggin = () => {
           This framework ensures that you energy is spread out evenly throughout
           the day and that heavy deliverables are done while simple everyday
           tasks are not forgotten.
+        </span>
+        <span>
+          Once this framework is enabled, return to the home page and the 1-3-5
+          rule will be imposed.
         </span>
       </div>
       <br />
@@ -74,10 +114,48 @@ const Pluggin = () => {
           </li>
         </ol>
         <span>
+          Once the technique is enabled, return to the home page and a timer
+          icon will appear next to the trash bin icon. Click on it to activate
+          the timer.
+        </span>
+        <span>
           Do be warned, a pleasant ringing tone will emanate from your computer
           when the time is up.
         </span>
       </div>
+      <br />
+      <h3 style={{ textDecoration: "underline" }}>Customisation</h3>
+      <span>
+        Fine tune the duration of the pomodoro timer in minutes. The maximum
+        duration is 60 minutes.
+      </span>
+      <br />
+      <br />
+      <Space
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-start",
+        }}
+      >
+        <form>
+          <input
+            type="number"
+            value={tempTime}
+            onChange={handleChange}
+            min="0"
+          />
+        </form>
+        <Button
+          type="primary"
+          onClick={changePomoTime}
+          shape="round"
+          size="medium"
+          style={{ width: "150%" }}
+        >
+          Set
+        </Button>
+      </Space>
       <br />
       <hr />
     </Fragment>
